3fde5f3fc8dfad51a828e362e67f08dbe04a8ba8
Saecki <tobiasschmitz2001@gmail.com>
2021-08-16 22:04:12 +0200

    install using cargo

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-cargo build --release
-sudo cp target/release/alco /usr/local/bin/
+cargo install --path .
 
 case "$SHELL" in
-    *bash)
-    echo "creating a completion script for bash"
-    /usr/local/bin/alco -g "bash" | sudo tee /etc/bash_completion.d/alco > /dev/null
-    ;;
     *zsh)
     echo "creating a completion script for zsh"
-    /usr/local/bin/alco -g "zsh" | sudo tee /usr/share/zsh/site-functions/_alco > /dev/null
+    ~/.cargo/bin/alco -g "zsh" > ~/.config/zsh/functions/_alco
     ;;
     *)
     echo "create a completion script for your shell manually by running 'alco --generate-completion <shell>'"
